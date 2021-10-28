dd5f79a17dc31f21247af248843e4753037809aa
Andrey_Del_Bello <andrey.delbello2002@gmail.com>
2021-10-28 19:01:35 +0000

    Resolução exercício 5

--- Principal.java
+++ Principal.java
@@ -10,6 +10,13 @@ public class Principal{
     novo.setAdicionalPreco(0.2*novo.getPreco());
     System.out.println(novo.getPreco()+novo.getAdicionalPreco());
 
+    System.out.println("Imovel Usado: ");
 
+    velho.setEndereco("Vitor Eusébio da Silva");
+    velho.setPreco(230000);
+
+    velho.setDescontoPreco(0.1*velho.getPreco());
+    System.out.println("Preço Imóvel velho: "+(velho.getPreco()-velho.getDescontoPreco()));
+    
     }
 }
